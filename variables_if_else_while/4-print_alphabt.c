85b9783a2906d365fad28e716269f6e1eabb3117
amil <abeshiri.ab@gmail.com>
2025-02-28 11:40:09 +0000

    4ready

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -9,11 +9,14 @@
  */
 int main(void)
 {
-char x;
-for (x = 'a'; x <= 'z'; x++)
-{
-if (x != 'q' && x != 'e')
-{putchar(x); }
-putchar ('\n');
-return (0);
+	char x;
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		if (x != 'q' && x != 'e')
+		{
+			putchar(x);
+		}
+	}
+	putchar ('\n');
+	return (0);
 }
