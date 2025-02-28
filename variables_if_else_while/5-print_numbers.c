e96084d48ccdcdd78baa3eeb7eba3daaf133ca0e
amil <abeshiri.ab@gmail.com>
2025-02-28 11:57:23 +0000

    5ready

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -Entry point
+ *
+ * Description: this code for print text on screen 
+ *
+ * Return Always 0 (Succes)
+ */
+int main(void)
+{
+char (x);
+for (x = 0; x <= 9; x++)
+return  (0);
+}
