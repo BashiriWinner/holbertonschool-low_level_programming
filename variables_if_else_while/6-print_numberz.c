deee4dc5d61f062ad912912583a043b6ca7558a7
amil <abeshiri.ab@gmail.com>
2025-02-28 13:25:19 +0000

    6ready

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -Entry point
+ *
+ * Description: this code for print text on screen
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	{int x = '0';
+
+        for(x = '0'; x<= '9'; x++)
+
+	{
+		putchar(x + '0');
+	}
+  	putchar ('\n')
+	return (0); 	
