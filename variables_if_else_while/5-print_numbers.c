76644a1a1900679440133143554a012ed204335c
amil <abeshiri.ab@gmail.com>
2025-02-28 12:52:25 +0000

    5ready

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
-* main -Entry point
+* main - Entry point
 *
-* Description: this code for print text on screen 
+* Description: this code for print text on screen
 *
-* Return Always 0 (Succes)
+* Return: Always 0 (Succes)
 */
 int main(void)
-{char x = '0';
-for (x = 0; x <= 9; x++)
-       {putchar(x); }
-	putchar ('\n');	
-return (0);
+{
+	char x = '0';
+
+	for (x = 0; x <= 9; x++)
+	{
+		putchar(x + '0');
+	}
+	putchar ('\n');
+	return (0);
 }
