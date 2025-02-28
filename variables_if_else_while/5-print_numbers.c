b75931307bed3096d241f38325d3743df4963eba
amil <abeshiri.ab@gmail.com>
2025-02-28 12:13:12 +0000

    5ready

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -3,13 +3,12 @@
 /**
  * main -Entry point
  *
- * Description: this code for print text on screen 
+*Description: this code for print text on screen 
  *
  * Return Always 0 (Succes)
  */
 int main(void)
-{
-char (x);
+{char x = '0';
 for (x = 0; x <= 9; x++)
 return  (0);
 }
