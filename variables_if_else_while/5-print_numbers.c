e2f34500410add61318dee20ba8f26cb38cf85b8
BashiriWinner <abeshiri.ab@gmail.com>
2025-02-28 16:41:11 +0400

    Update 5-print_numbers.c

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main -Entry point
- *
-*Description: this code for print text on screen 
- *
- * Return Always 0 (Succes)
- */
+* main -Entry point
+*
+* Description: this code for print text on screen 
+*
+* Return Always 0 (Succes)
+*/
 int main(void)
 {char x = '0';
 for (x = 0; x <= 9; x++)
        {putchar(x); }
 	putchar ('\n');	
-return  (0);
+return (0);
 }
