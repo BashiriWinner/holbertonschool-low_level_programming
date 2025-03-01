df595f0a0ae35d7f407fdf9ef72d0244eb6560b9
amil <abeshiri.ab@gmail.com>
2025-03-01 19:58:08 +0000

    8ready

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,28 +1,18 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 /**
  * main - Entry point
  *
- * Description: this code for print text on screen
+ * Description: 'this code for print text on screen'
  *
  * Return: Always 0 (Success)
- *
  */
 int main(void)
-{
-	char (x);
-	char (y);
-
-	for (x = '0'; x <= '9'; x++);
-
-	{
-		putchar(x);
-	}
-
-	for (y = 'a'; y <= 'f'; y++);
-
-	{putchar(y); }
-
-	putchar('\n');
-	return (0)
-}
+{char x;
+char y;
+for (x = '0'; x <= '9'; x++)
+{putchar(x); }
+for (y = 'a'; y <= 'f'; y++)
+{putchar(y); }
+putchar('\n');
+return (0); }
