9b6f9f406c44f3f20710ffefa7cd4afdf46ee342
amil <abeshiri.ab@gmail.com>
2025-03-03 17:26:21 +0000

    2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -4,16 +4,15 @@
  * print_alphabet - Entry point
  *
  * Return: Always 0 (Success)
- *
  */
-void print_alphabet_10x(void)
+void print_alphabet(void)
 {
 	char a[] = "abcdefghijklmnopqrstuvwxyz\n";
 	int i = 0;
 
 	while (a[i])
 	{
-		_putchar (a[i]);
+		_putchar(a[i]);
 		i++;
 	}
 }
