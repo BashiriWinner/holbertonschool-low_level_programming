b0fbe697ca4399cea007fe6df1bf356e08590dd4
amil <abeshiri.ab@gmail.com>
2025-03-03 12:06:22 +0000

    1ready

---
+++ functions_nested_loops/1-main.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void);
+
+{
+
+	char a[] = "abcdefghijklmnopqrstuvwxyz\n";
+        int i = 0; 
+
+        while (a[i])
+
+	    {
+		_putchar(a[i]);
+		i++;
+	}
+
+}
