e64439c4302862ff7c5e20caef488e262e92b963
amil <abeshiri.ab@gmail.com>
2025-03-04 11:29:38 +0000

    9-times_table.c

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,43 +1,44 @@
 #include "main.h"
 
 /**
- * jack_bauer - Entry point
+ * times_table - Entry point
  *
  * Return: Always 0 (Success)
  */
 void times_table(void)
 {
-	int i = 0, j = 0;
+int i = 0, j = 0;
 
-	while (i < 24)
-	{
-		while (j < 60)
-		{
-			if (i < 10)
-			{
-				_putchar ('0');
-				_putchar (i % 10 + '0');
-			}
-			else
-			{
-				_putchar (i / 10 + '0');
-				_putchar (i % 10 + '0');
-			}
-			_putchar (':');
-			if (j < 10)
-			{
-				_putchar ('0');
-				_putchar (j % 10 + '0');
-			}
-			else
-			{
-				_putchar (j / 10 + '0');
-				_putchar (j % 10 + '0');
-			}
-			_putchar ('\n');
-			j++;
-		}
-		j = 0;
-		i++;
-	}
+while (i < 10)
+{
+while (j < 10)
+{
+if (i * j < 10)
+{
+_putchar((i * j) % 10 + '0');
+}
+else
+{
+_putchar((i * j) / 10 + '0');
+_putchar((i * j) % 10 + '0');
+}
+if (j != 9)
+{
+_putchar(',');
+if (i * (j + 1) <= 9)
+{
+_putchar(' ');
+_putchar(' ');
+}
+else
+{
+_putchar(' ');
+}
+}
+j++;
+}
+_putchar('\n');
+i++;
+j = 0;
+}
 }
