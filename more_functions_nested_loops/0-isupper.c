7edd01320afb8a73e5c8ea052f15b5ca76c0cf0f
amil <abeshiri.ab@gmail.com>
2025-03-05 09:03:26 +0000

    0-isupper.c

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,20 +1,19 @@
 #include "main.h"
 
 /**
- * _isupper  - Entry point
+ * _isupper - Entry point
  * @c: variable
  *
  * Return: Always 0 (Success)
- *
- * int  _isupper(int c)
+ */
+int _isupper(int c)
 {
-        if (c >= 65 && c <= 90)
-        {
-              return (1);
+	if(c >= 65 && c <= 90)
+	{
+		return (1);
 	}
-
-             else
-        {
-                return (0);
+	else
+	{
+		return (0);
 	}
 }
