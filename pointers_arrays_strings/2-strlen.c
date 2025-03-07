91f64c33ac391ec91485d303f48ca4ba5fd12037
amil <abeshiri.ab@gmail.com>
2025-03-07 13:40:57 +0000

    2-strlen.c

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - Entry point
+ * @s: Description for n
+ *
+ * Return: Always 0 (Success)
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s != '\n')
+	{
+		s++;
+		i++;
+	}
+	return (i);
+}
