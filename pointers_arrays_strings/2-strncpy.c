2f4bb027da9268b279585e27006ec4f54df814e2
amil <abeshiri.ab@gmail.com>
2025-03-10 08:52:55 +0000

    2-strncpy.c

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * *_strncpy - Entry point 
+ * @dest: input of function
+ * @src: src
+ * @n: n
+ *
+ * Return: Always 0
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	
+	int a = 0, b = 0, c = 0;
+
+	while (a < n)
+	{
+		dest[a] = '\0';
+		a++;
+	}
+
+	while (src[c] != '\0')
+	{
+		c++;
+	}
+
+	if (n > c)
+	{
+		n = c;
+	}
+
+	while (b < n)
+	{
+		if (src[0] == '\0')
+		{
+			break;
+		}
+
+		*(dest + b) = *(src + b);
+		b++;
+	}
+
+	return (dest);
+}
