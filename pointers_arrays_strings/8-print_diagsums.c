6f2bd42ca133fd1b0d5f846e9d2e6945b2100805
amil <abeshiri.ab@gmail.com>
2025-03-12 09:49:52 +0000

    8-print_diagsums.c

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -8,10 +8,7 @@
  */
  void print_diagsums(int *a, int size)
  {
-     int i;
-     int j;
-     sum1 = 0;
-     sum2 = 0;
+     int i, int j, sum1 = 0, sum2 = 0;
      for(i = 0; i <= (size * size); i = i + size + 1)
      {
          sum1 = sum1 + a[i];
