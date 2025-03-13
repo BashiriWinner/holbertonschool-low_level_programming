12e411dccf05ebdffd1592a62573a1f35fb53b04
amil <abeshiri.ab@gmail.com>
2025-03-13 09:35:16 +0000

    2-strlen_recursion.c

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -6,9 +6,9 @@
  */
 int _strlen_recursion(char *s)
 {
-	if (*s == '\n')
+	if (*s ! == '\n')
 	{
-		return (1 + _strlen_recursion(char + 1));
+		return (1 + _strlen_recursion(s + 1));
 	}
 	return (0);
 }
