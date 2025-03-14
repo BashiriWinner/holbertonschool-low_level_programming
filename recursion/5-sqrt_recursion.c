0413302340db1936851b7dd7ff5d41f10bea7cf9
amil <abeshiri.ab@gmail.com>
2025-03-14 10:31:12 +0000

    5-sqrt_recursion.c

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - check the code
+ * @n: square
+ * Return: Always 0
+ */
+int _sqrt_recursion(int n);
+{
+	if (0 > n)
+	{
+		return (-1);
+	}
+	else if (0 == n)
+	{
+		return (0);
+	}
+	else
+	{
+		return (_sqrt(n, 1));
+	}
+}
+/**
+ * _sqrt - check the code
+ * @x: x
+ * @y: y
+ * Return: Always 0.
+ */
+int _sqrt(int y, int x)
+{
+	if (x * x == y)
+		return (x);
+	else if (x * x > y)
+		return (-1);
+	else
+		return (_sqrt(y, x + 1));
+}
