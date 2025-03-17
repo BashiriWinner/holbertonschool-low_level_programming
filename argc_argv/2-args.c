37720fd8f55955976a0ac1fa6a5f57f1b0cd5ca4
BashiriWinner <abeshiri.ab@gmail.com>
2025-03-17 15:07:15 +0400

    2-args.c

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,13 +1,19 @@
 #include "main.h"
+#include <stdio.h>
 /**
+ * main - check the code
+ * @argc: argc
+ * @argv: argv
+ * Return: Always 0.
  */
 int main(int argc, char *argv[])
-{	int i;
-	char *p;
+{
+	int i = 0;
 
-	for(i = 1; i > argc; i++){
-		p = argv[argc + i - 1];
-		printf("%s\n",p);
-	};
-	return(0);
+	while (i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
 }
