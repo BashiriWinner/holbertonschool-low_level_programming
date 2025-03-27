51ed1e6bb0020f45793e4c6dc5a31f51226a4a82
BashiriWinner <abeshiri.ab@gmail.com>
2025-03-27 15:43:27 +0400

    0-print_name.c

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,13 +1,14 @@
-/**
- * print_name - prints name
- * @name: name to print (char *)
- * @f: callback function (pointer to the void func.)
- *
- * Return: nothing
- */
+#include <stdio.h>
+#include "function_pointers.h"
 
+/**
+ * print_name - prints a name as is
+ * @name: name of the person
+ * @f: name of pointer
+ *
+ * Return: Nothing.
+ */
 void print_name(char *name, void (*f)(char *))
 {
-	if (f)
-		f(name);
+f(name);
 }
