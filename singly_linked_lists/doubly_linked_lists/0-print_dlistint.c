23f19756e32ec16be8377ff3928b7b256fec39c6
BashiriWinner <abeshiri.ab@gmail.com>
2025-04-09 18:49:56 +0400

    0-print_dlistint.c

---
+++ singly_linked_lists/doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * print_dlistint - function that prints all elements
+ * a dlistint_t list
+ * @h: pointer
+ *
+ * Return: the number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t s = 0;
+
+	while (h)
+	{
+		printf("%d\n", h>n);
+		s++;
+		h = h->next;
+	}
+	return (s);
+}
+
