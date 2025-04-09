e9fd4b3eb94b025ae21916c52dbfa22017e53777
BashiriWinner <abeshiri.ab@gmail.com>
2025-04-09 15:52:01 +0400

    3-add_node_end.c

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -9,42 +9,41 @@
  * @str: String to be added to the new node
  * Return: Adrress of the new element, or NULL if it failed
  */
+typedef struct list_s
+{
+    char *str;
+    struct list_s *next;
+    list_t;
+}
+
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node;
-	int lens = 0;
-	int i = 0;
-
-	if (str == NULL)
-		return (NULL);
-
-	while (str[lens] != '\0')
-	{
-		lens++;
-	}
-	new_node = malloc(sizeof(list_t));
+	list_t *new_node = (list_t *)malloc(sizeof(list_t));
 	if (new_node == NULL)
-		return (NULL);
-
+	return (NULL);
+	
 	new_node->str = strdup(str);
 	if (new_node->str == NULL)
 	{
 		free(new_node);
 		return (NULL);
 	}
-	new_node->len = lens;
+	
 	new_node->next = NULL;
+	
 	if (*head == NULL)
 	{
 		*head = new_node;
 		return (new_node);
 	}
-	while (temp->next != NULL)
+	
+	list_t *current = *head;
+	while (current->next != NULL)
 	{
-		temp = temp->next;
+		current = current->next;
 	}
-
 	current->next = new_node;
 	
 	return (new_node);
 }
+
