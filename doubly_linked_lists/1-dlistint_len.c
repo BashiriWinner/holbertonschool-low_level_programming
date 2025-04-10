519d8d8deb2dec693e2d6b767849649cbb7656d0
BashiriWinner <abeshiri.ab@gmail.com>
2025-04-10 13:30:54 +0400

    1-dlistint_len.c

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include "lists.h"
 /**
- * dlistint_len - function that returns the number of elements 
+ * dlistint_len - function that returns the number of elements
  * in a linked dlistint_t list
  * @h: pointer named head and this first node
  * Return: the len of nodes
@@ -9,6 +9,7 @@
 size_t dlistint_len(const dlistint_t *h)
 {
 	size_t count = 0;
+
 	while (h != NULL)
 	{
 		count++;
