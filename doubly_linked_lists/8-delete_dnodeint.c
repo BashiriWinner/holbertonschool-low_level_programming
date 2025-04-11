946d0fe2e81b3aab6793d0dbd1df650037390fbe
BashiriWinner <abeshiri.ab@gmail.com>
2025-04-11 19:16:04 +0400

    8-delete_dnodeint.c

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -10,34 +10,34 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *current = *head;
-    unsigned int current_index = 0;
+dlistint_t *current = *head;
+unsigned int current_index = 0;
 
-    if (*head == NULL)
-        return (-1);
+if (*head == NULL)
+return (-1);
 
-    if (index == 0)
-    {
-        *head = (*head)->next;
-        if (*head != NULL)
-            (*head)->prev = NULL;
-        free(current);
-        return (1);
-    }
+if (index == 0)
+{
+*head = (*head)->next;
+if (*head != NULL)
+(*head)->prev = NULL;
+free(current);
+return (1);
+}
 
-    while (current != NULL && current_index < index)
-    {
-        current = current->next;
-        current_index++;
-    }
+while (current != NULL && current_index < index)
+{
+current = current->next;
+current_index++;
+}
 
-    if (current == NULL)
-        return (-1);
+if (current == NULL)
+return (-1);
 
-    if (current->prev != NULL)
-        current->prev->next = current->next;
-    if (current->next != NULL)
-        current->next->prev = current->prev;
-    free(current);
-    return (1);
+if (current->prev != NULL)
+current->prev->next = current->next;
+if (current->next != NULL)
+current->next->prev = current->prev;
+free(current);
+return (1);
 }
