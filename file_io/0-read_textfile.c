b5a4b9fa61932f4e987862be87d18d49d9b6ffc2
BashiriWinner <abeshiri.ab@gmail.com>
2025-04-17 12:45:05 +0400

    0-read_textfile.c

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,52 +1,31 @@
-#include <fcntl.h>
-#include <unistd.h>
-#include <stddef.h>
-#include <stdlib.h>
-#include <stdio.h>
 #include "main.h"
-
+#include <stdlib.h>
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output
- * @filename: file to read
- * @letters: the number of letters it should read and print
- *
- * Return:
- *	the actual number of letters it could read and print
- *	if the file can not be opened or read, return 0
- *	if filename is NULL return 0
- *	if write fails or does not write the expected amount of bytes, return 0
+ * read_textfile - reads a text file and prints it
+ * @filename: pointer
+ * @letters: number of letters
+ * Return: the actual number of letters it could read and print
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	size_t pchars;
-	char *buf;
+	char *fptr;
+	ssize_t r, wr, fd;
 
-	if (filename == NULL)
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	fptr = malloc(sizeof(ssize_t) * letters);
+	if (fptr == NULL)
 		return (0);
 
-	fd = open(filename, O_RDWR);
+	r = read(fd, fptr, letters);
 
-	if (fd < 0)
+	wr = write(STDOUT_FILENO, fptr, r);
+
+	if (wr == -1)
 		return (0);
 
-	buf = (char *) malloc(letters * sizeof(char) + 1);
-
-	if (buf == NULL)
-		return (0);
-
-	pchars = read(fd, buf, letters);
+	free(fptr);
 	close(fd);
-
-	if (!pchars)
-		return (0);
-
-	buf[pchars] = '\0';
-	pchars = write(STDOUT_FILENO, buf, pchars);
-
-	if (!pchars)
-		return (0);
-
-	return (pchars);
+	return (wr);
 }
